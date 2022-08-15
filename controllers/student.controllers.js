6d15c848674c563eacba556586e44fa1e1d4a30f
Ethel3 <akrasiethel4@gmail.com>
2022-08-15 19:03:32 +0000

    admit new student

---
+++ controllers/student.controllers.js
@@ -0,0 +1,9 @@
+//admit new students
+export const admitStudent = (req, res)=>{
+    res.send("Add new student endpoint!");
+}
+
+// get all students
+export const getAllStudent = (req, res)=>{
+    res.send("All students in the school");
+}
