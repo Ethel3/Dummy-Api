dd0bcb89de3ca7933e2a1fc9a29789375de64ac5
Ethel3 <akrasiethel4@gmail.com>
2022-08-15 19:03:16 +0000

    get all students

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,21 +1,21 @@
 import express, { application } from "express";
+import { admitStudent, getAllStudent } from "../controllers/student.controllers.js";
 
 const StudentRoute = express.Router();
 
 // Admit new students
-StudentRoute.post("/", (req, res)=>{
-    res.send("Add new student endpoint!");
-})
+StudentRoute.post("/", admitStudent)
 
 // Get all students
-StudentRoute.get("/", (req, res)=>{
-    res.send("All students in the school");
-})
+StudentRoute.get("/", getAllStudent)
 
 //Update Student details
 StudentRoute.put("/", (req, res)=>{
     res.send("Updated student details");
 })
 
-
+//Dismiss student
+StudentRoute.delete("/", (req, res)=>{
+    res.send("Dismissed student")
+})
 export default StudentRoute
