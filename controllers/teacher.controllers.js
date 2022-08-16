82a55e8f61776cea783fa1fb145baa6414f885a6
Ethel3 <akrasiethel4@gmail.com>
2022-08-16 22:36:24 +0000

    Updated get all teachers controllers

--- controllers/teacher.controllers.js
+++ controllers/teacher.controllers.js
@@ -2,3 +2,8 @@
 export const adminTeacher = (req, res)=>{
     res.send("Admitted new teacher")
 }
+
+//get all teachers
+export const getAllTeacher = (req, res)=>{
+    res.send("These are all the staff members")
+}
