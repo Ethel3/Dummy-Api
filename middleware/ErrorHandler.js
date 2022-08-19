c108db595fbb4cbc5ea218f03ff75d6b1fd7d4dc
Ethel3 <akrasiethel4@gmail.com>
2022-08-19 18:40:54 +0000

    Updated err handler

--- middleware/ErrorHandler.js
+++ middleware/ErrorHandler.js
@@ -1,3 +1,8 @@
 const Handler = (err, req, res, next)=> {
     const sc = err.statusCode || 500
+    const msg = err.message || "Something went wrong"
+    var stack = ""
+    if(ProcessingInstruction.env.NODE_ENV == "development"){
+        stack = err.stack
+    }
 }
