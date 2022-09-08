fad6d72e1da08c67b4746e0de21a82fb8cc6dacb
Ethel3 <akrasiethel4@gmail.com>
2022-09-08 22:45:05 +0000

    Imported class model

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import StudentRoute from "./routes/student.routes.js";
 import TeacherRoute from "./routes/teacher.routes.js";
+import classModels from "./models/class.models.js";
 import DBCONNECT from "./dbconnections.js";
 import bodyParser from "body-parser";
+
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json())
 app.use('/student', StudentRoute)
 app.use('/teacher', TeacherRoute)
+app.use('/class', classModels)
 
 // home page
 app.get("/" , (req, res, next) => {
