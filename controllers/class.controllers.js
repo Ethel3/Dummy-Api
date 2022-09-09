4bd7b74b8995756d13fd629992cc358ce84bd9b8
Ethel3 <akrasiethel4@gmail.com>
2022-09-09 22:09:02 +0000

    Updated class controllers

--- controllers/class.controllers.js
+++ controllers/class.controllers.js
@@ -1,17 +1,17 @@
 import express from "express";
-import classModels from "../models/class.models.js";
+import ClassModels from "../models/class.models.js";
 
 
-// export const getClass = async (req, res, next)=>{ 
-//     try{
-//     const class = await classModels.create(req.body)
-//     res.status(200).json({
-//         success: true,
-//         total: class.length,
-//         data: class,
-//         message: "Total class number "
-//     });
-// } catch (error){
-//     next(error);
-//   }
-// }
+export const getClass = async (req, res, next)=>{ 
+    try{
+    const classes = await ClassModels.find(req.body)
+    res.status(200).json({
+        success: true,
+        total: classes.length,
+        data: classes,
+        message: "Total class number "
+    });
+} catch (error){
+    next(error);
+  }
+}
