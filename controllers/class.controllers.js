23fed221e68e884ece6c74ad3333d8c5ae2885dc
Ethel3 <akrasiethel4@gmail.com>
2022-09-10 21:57:34 +0000

    Updated class controllers

--- controllers/class.controllers.js
+++ controllers/class.controllers.js
@@ -15,3 +15,17 @@ export const getClass = async (req, res, next)=>{
     next(error);
   }
 }
+//updating classes
+export const updateClass = async (req, res, next)=>{ 
+    try{
+    const classes = await ClassModels.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    res.status(200).json({
+        success: true,
+        total: classes.length,
+        data: classes,
+        message: "Updated  class number "
+    });
+} catch (error){
+    next(error);
+  }
+}
