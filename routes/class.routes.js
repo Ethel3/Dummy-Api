a058189cdf25a1defeadfd27418392f524d88c1f
Ethel3 <akrasiethel4@gmail.com>
2022-09-12 22:31:15 +0000

    Imported Class methods

--- routes/class.routes.js
+++ routes/class.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getClass } from "../controllers/class.controllers.js";
+import { deleteClass, getClass, updateClass } from "../controllers/class.controllers.js";
 
 const ClassRoute = express.Router();
 
 // Get all students
 ClassRoute.get("/", getClass)
+//Update class 
+ClassRoute.put("/", updateClass)
+//delete class
+ClassRoute.delete("/", deleteClass)
 
 
 export default ClassRoute
