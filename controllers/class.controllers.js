631cdc8e6048c85fd182d221838d4992fb33ace7
Ethel Akrasi Akosua <88440439+Ethel3@users.noreply.github.com>
2022-09-18 00:37:11 +0100

    Update class.controllers.js

--- controllers/class.controllers.js
+++ controllers/class.controllers.js
@@ -15,6 +15,21 @@ export const getClass = async (req, res, next)=>{
     next(error);
   }
 }
+//creating new class
+export const createClass = async (req, res, next)=>{ 
+    try{
+    const classes = await ClassModels.find(req.body)
+    res.status(200).json({
+        success: true,
+        total: classes.length,
+        data: classes,
+        message: "Created new class number "
+    });
+} catch (error){
+    next(error);
+  }
+}
+
 //updating classes
 export const updateClass = async (req, res, next)=>{ 
     try{
