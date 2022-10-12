c3e1e1dd92be5d5c71af93f2af107a4717cf836c
Ethel3 <akrasiethel4@gmail.com>
2022-10-12 19:14:06 +0000

    Updated some functions

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -4,9 +4,10 @@ import UserSchema from "../models/user.models.js";
 // login user
 export const Login = async (req, res, next)=>{ 
     const { username } = req.body;
+
     try{
-    const user = await UserSchema.find({ username, isdeleted: false})
-    if (!user){
+    const user = await UserSchema.findOne({ username, isdeleted: false})
+    if (!user) {
         return res.status(400).json({
             success: false,
             message: "Incorrect Username or password"
@@ -14,9 +15,9 @@ export const Login = async (req, res, next)=>{
     }
    
 } catch (error){
-    next(error);
+    next(error)
   }
-}
+};
 
 // add user
 export const addUser = async (req, res, next)=>{
@@ -24,14 +25,17 @@ export const addUser = async (req, res, next)=>{
         const { fullname, username, email} = req.body;
         if (!fullname || !username || !email || !req.body.password){
         res.status(400)
-        next (createError("Please fill all fields", 400))   
+        next(CreateError("Please fill all fields", 400))   
         }
     
         //Existing user
-        const userExists = await UserSchema.find({email})
-        if (userExists){
-            res.status(401)// this should not work
-            next (createError("User already exists", 400))
+        const userExists = await UserSchema.findOne({ email })
+        if (userExists) {
+            res.status(409)// this should not work
+            next(CreateError("User already exists", 400))
         }
+      } catch (error){
+        next(error)
       }
     }
+    
