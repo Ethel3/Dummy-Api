96c74d398a36f859d27fd584d72fff5e90ae94d0
Ethel3 <akrasiethel4@gmail.com>
2022-10-13 15:28:00 +0000

    Created refreshToken

---
+++ controllers/getRefreshToken.controllers.js
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from "express";
+
+import UserSchema from "../models/user.models.js"
+import jwt from "jsonwebtoken"
+
+export const getRefreshToken = async (req, res, next) => {
+    try {
+
+        const cookies = req.cookies
+        if(!cookies?.jwt) return res.sendStatus(401) // unauthorized
+        const refreshToken = cookies.jwt
+        // console.log("token >>>>", refreshToken)
+        const user = await UserSchema.findOne({token: refreshToken})
+
+        // console.log(user)
+        if(!user){
+            console.log("No user found")
+            return res.sendStatus(403) // forbidden
+        }
+
+         const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET || "")
+
+        //  console.log("decoded >>>", decoded)
+
+         const { password, token, ...updated_user } = user._doc 
+
+         // now generate a new access token
+         const accessToken = jwt.sign({id: decoded.id }, process.env.JWT_SECRET || "", {expiresIn: "1d"})  
+        
+         updated_user.access_token = accessToken
+            // console.log(updated_user)
+            res.status(201).json({
+                success: true,
+                user: updated_user,
+                message: "new token generated"
+            })  
+            
+        
+    } catch (error) {
+        // console.log("Refresh error");
+        
+        next(error)
+    }
+}
+
