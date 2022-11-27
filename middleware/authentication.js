b8b11fc36fdc24e7930f9d1c6f9130812c3d752d
Ethel3 <akrasiethel4@gmail.com>
2022-11-27 21:24:15 +0000

    verifications

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
 import express from "express";
 import CreateError from "../utils/Error.js";
-
-export const verifyAccessToken = (req, res, next)=>{
-    
+export const verifyAccessToken = (
+    req, res, next
+)=>{
+    let token;
 }
