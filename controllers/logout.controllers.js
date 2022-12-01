2277d929b27d45b84d4b0892efdfc218ec2e078f
Ethel3 <akrasiethel4@gmail.com>
2022-12-01 22:20:25 +0000

    Making changes

--- controllers/logout.controllers.js
+++ controllers/logout.controllers.js
@@ -3,9 +3,25 @@ import bcrypt from "bcryptjs";
 import { CreateError } from "../utils/Error.js";
 import userModels from "../models/user.models.js";
 
+// register
+export const register = async (req, res, next) => {
+    const { username, email, password, name } = req.body;
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+    try {
+      const user = new user({
+        username,
+        email,
+        password: hash,
+        name,
+      });
+      await user.save();
+      res.status(201).json(user);
+    } catch (error) {
+      next(error);
+    }
+  };
 
 
 
 
-
-export default logOut;
